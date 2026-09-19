7074572d2d5a973f68805deeded7c96f33bf2a27
DanielSF39 <daniel.sologuren.falcon@gmail.com>
2026-09-19 18:23:44 -0500

    Implementar módulo JS e inicialización para animaciones de servicios

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//PUNTO DE ENTRADA PRINCIPAL
+
+import { initServices } from './modules/services.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //Inicialización de servicios y animaciones
+    initServices();
+
+    
+});
