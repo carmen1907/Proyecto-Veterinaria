66cd0aa05b83e005a8ff311ba415923644fd1d8f
Ryuz12 <neymarryuz123@gmail.com>
2026-09-19 23:00:09 -0500

    Se agregó el script de la sección de cita js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 //PUNTO DE ENTRADA PRINCIPAL
 
 import { initServices } from './modules/services.js';
+import { initAppointmentForm } from './modules/appointmentForm.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 
     //Inicialización de servicios y animaciones
     initServices();
+    
+    //Inicialización de formulario de citas y validaciones
+    initAppointmentForm();
 
     
 });
